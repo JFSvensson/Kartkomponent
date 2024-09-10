b1d246a24423df5b644c302afe4f895a10e6788d
Fredrik Svensson <svensson.jf@gmail.com>
2024-09-10 12:28:41 +0200

    Cleanup and fixes

--- kartkomponent.js
+++ kartkomponent.js
@@ -1,20 +1,16 @@
 class KartKomponent extends HTMLElement {
   constructor() {
-      super();
-      
-      // Skapa en div som ska innehålla kartan (utanför Shadow DOM)
+      super();      
       this.mapContainer = document.createElement('div');
       this.mapContainer.setAttribute('id', 'map');
       this.mapContainer.style.height = '400px';
       this.mapContainer.style.width = '100%';
-
-      // Lägg till kartcontainern i vanliga DOM-trädet
       this.appendChild(this.mapContainer);
   }
 
   connectedCallback() {
-    // Ladda data från en extern JSON-fil
-    fetch('data/kulturstigData.json')
+    const dataUrl = this.getAttribute('data-url');
+    fetch(dataUrl)
     .then(response => response.json())
     .then(data => {
         this.createMap(data.center, data.zoom, data.markers);
@@ -22,10 +18,8 @@ class KartKomponent extends HTMLElement {
   }
 
   createMap(center, zoom, markers) {
-      // Skapa kartan och ställ in den centralt på en specifik plats
       const map = L.map(this.mapContainer).setView(center, zoom);
 
-      // Lägg till OpenStreetMap tiles
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
@@ -34,16 +28,7 @@ class KartKomponent extends HTMLElement {
         const leafletMarker = L.marker([marker.lat, marker.lng]).addTo(map);
         leafletMarker.bindPopup(`<b>${marker.popup}</b>`);
       });
-
-      // Tooltip visas vid hover
-      marker.bindTooltip('Hover-text för markering').openTooltip();
-      
-      // Hantera klick på markeringen för att länka till en annan sida
-      marker.on('click', () => {
-          window.location.href = 'https://example.com/information';
-      });
   }
 }
 
-// Definiera webbkomponenten
 customElements.define('svenssonom-kartkomponent', KartKomponent);
